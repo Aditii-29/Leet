a8393dcbda1f1c0e0e484006ec029abf6a8398d1
Aditi <aditisinghjuhi@gmail.com>
2026-09-19 19:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (16.60%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -10,26 +10,54 @@
  * };
  */
 class Solution {
-    private:
-    void dfs(TreeNode*node,int level,vector<vector<int>>&res){
-        if(!node)return;
-        if(level==res.size()){
-            res.push_back({});
-        }
-        if(level % 2==0){
-            res[level].push_back(node->val);
-        }else{
-        res[level].insert(res[level].begin(),node->val);
+    //Through recursion
+    // private:
+    // void dfs(TreeNode*node,int level,vector<vector<int>>&res){
+    //     if(!node)return;
+    //     if(level==res.size()){
+    //         res.push_back({});
+    //     }
+    //     if(level % 2==0){
+    //         res[level].push_back(node->val);
+    //     }else{
+    //     res[level].insert(res[level].begin(),node->val);
 
-        }
+    //     }
 
-        dfs(node->left,level+1,res);
-        dfs(node->right,level+1,res);
-    }
+    //     dfs(node->left,level+1,res);
+    //     dfs(node->right,level+1,res);
+    // }
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        // vector<vector<int>>res;
+        // dfs(root,0,res);
+        // return res;
+
+        //Using queue
         vector<vector<int>>res;
-        dfs(root,0,res);
+        if(!root) return res;
+
+        queue<TreeNode*>q;
+        q.push(root);
+        bool leftToRight=true;
+
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>level(n);
+
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                int index=leftToRight?i:(n-1-i);
+                level[index]=node->val;
+
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+
+            }
+            res.push_back(level);
+            leftToRight=!leftToRight;
+        }
         return res;
 
     }
